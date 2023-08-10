f5c22ef5c2e2279e50d0b1bc5f5fecdbc96c5c77
EmilKietzman <135075052+EmilKietzman@users.noreply.github.com>
2023-08-10 10:37:19 +0200

    Initialize fix/lessons

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { FormContainer, Loader } from "./components";
+import { useMaginkContract } from "./hooks";
+
+function App() {
+  const { magink } = useMaginkContract();
+
+  if (!magink) return <Loader message="Loading app..." />
+
+  return (
+    <Routes>
+      <Route index element={<FormContainer />} />
+    </Routes>
+  );
+}
+
+export default App;
