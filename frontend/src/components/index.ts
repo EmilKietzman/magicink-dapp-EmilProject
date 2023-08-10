721fb05928c0367f8452643a77d5bd79b9c3374f
EmilKietzman <135075052+EmilKietzman@users.noreply.github.com>
2023-08-10 10:37:11 +0200

    Initialize dependabot/cargo/contract/ink_metadata-4.2.0

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,6 @@
+export * from "./DryRunResult";
+export * from "./Header";
+export * from "./LinksOverview";
+export * from "./FormContainer";
+export * from "./Loader";
+export * from "./NewUserGuide";
